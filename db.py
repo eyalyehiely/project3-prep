b0423ecda53507a9ef7d687a13fd0d2de6c3fc48
eyalyehiely <eyalwork0@gmail.com>
2024-03-17 20:07:05 +0200

    second commit

--- db.py
+++ db.py
@@ -1,12 +1,29 @@
 import psycopg2
 def query(sql):
-    with psycopg2.connect(
-        database="postgres",
-        host="localhost",
-        user="postgres",
-        password="postgres",
-        port="5432") as conn:
-        cur=conn.cursor()
-        cur.execute(sql)
-        return cur.fetchall()
+    try:
+        with psycopg2.connect(
+            database="postgres",
+            host="localhost",
+            user="postgres",
+            password="postgres",
+            port="5432") as conn:
+                
+                cur=conn.cursor()
+                cur.execute(sql)
+                try:
+                    return cur.fetchall()
+                except Exception as e:
+                    print(e)          
+    except Exception as e:
+        print(e)
       
+
+
+def setup():
+    query("CREATE TABLE IF NOT EXISTS services (id INT PRIMARY KEY, name TEXT)")
+
+
+
+if __name__ =='__main__':
+    setup()
+
